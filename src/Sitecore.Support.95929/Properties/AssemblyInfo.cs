4b381229b65bf7edba1ebee5c308c7bacd41d3ef
Alexey Tupkalo <altu@sitecore.net>
2017-11-21 15:53:29 +0200

    Patch 95929 commit with original code

---
+++ src/Sitecore.Support.95929/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.95929")]
+[assembly: AssemblyProduct("Sitecore.Support.95929")]
+[assembly: ComVisible(false)]
